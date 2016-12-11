9894fecee5ad99e2e0ca9c0466aec9e815ce1ae8
Colin Gillespie <csgillespie@gmail.com>
2016-12-11 22:58:06 +0000

    Removed nesting from if statements

--- R/fit_gambin.R
+++ R/fit_gambin.R
@@ -34,25 +34,24 @@
 #' # gambin parameters based on a standardized sample size of 1000 individuals
 #' stand_fit <- replicate(20, fitGambin(moths, 1000)$Alpha) #may take a while on slower computers
 #' print(c(mean = mean(stand_fit), sd = sd(stand_fit)))
-  #' @export
-fitGambin <-
-function(abundances, subsample = 0)
+#' @export
+fitGambin <- function(abundances, subsample = 0)
 {
   # for the GamBin function, all the abundances are binned into octaves (on log base 2)
-
+  
   # test for NA's in the data (and that it is a numeric vector)
   Dataname <- deparse(substitute(abundances))
   
-  if(is.vector(abundances) & is.numeric(abundances))
-  {
-  	mydata <- create_octaves(abundances, subsample)
+  if(is.vector(abundances) && is.numeric(abundances)) {
+    mydata <- create_octaves(abundances, subsample)
+  } else  if(is.data.frame(abundances)) {
+    names(abundances) <- tolower(names(abundances))
+    if(!("species" %in% names(abundances) && "octave" %in% names(abundances))) {
+      stop("abundances must be a numeric vector or a data.frame created by create_octaves")	
+    }
+    mydata <- abundances[c("octave", "species")]
   } else {
-  	if(is.data.frame(abundances))
-  	{
-  		names(abundances) <- tolower(names(abundances))
-  		if(!("species" %in% names(abundances) & "octave" %in% names(abundances))) stop("abundances must be a numeric vector or a data.frame created by create_octaves")	
-  		mydata <- abundances[c("octave", "species")]
-  	} else stop("abundances must be a numeric vector or a data.frame created by create_octaves")	
+    stop("abundances must be a numeric vector or a data.frame created by create_octaves")	
   }
   
   val <- optimise(logLik_gamBin, interval = c(0,15), mydata = mydata)
@@ -72,12 +71,10 @@ function(abundances, subsample = 0)
   res$logLik <- logLik
   
   res$fitted.values <- gambin_exp(res$Alpha, max(mydata$octave), sum(mydata$species))
-
-
   res$Data <- mydata
   if (subsample == 0)
     res$Dataname <- Dataname else
-    res$Dataname <- paste(subsample, "individuals sampled from", Dataname)
+      res$Dataname <- paste(subsample, "individuals sampled from", Dataname)
   if(length(res$Dataname) > 1) res$Dataname <- "unnamed abundance vector"
   
   res$MaxOctave <- max(mydata$octave)
