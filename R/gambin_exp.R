117c2ba27771372504612a5d33aeb8ab19c5b216
Colin Gillespie <csgillespie@gmail.com>
2016-12-13 16:49:35 +0000

    Simplify code

--- R/gambin_exp.R
+++ R/gambin_exp.R
@@ -1,9 +1,6 @@
 #' @rdname dgambin
 #' @export
-gambin_exp <-
-function(alpha, maxoctave, total_species)
+gambin_exp = function(alpha, maxoctave, total_species)
 {
-  exp <- dgambin(0:maxoctave, alpha, maxoctave)
-  exp <- exp * total_species 
-  return(exp)
+  dgambin(0:maxoctave, alpha, maxoctave) * total_species
 }
