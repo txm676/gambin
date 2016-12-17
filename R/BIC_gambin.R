774e82fc1554a7474b3b389788176f424c2225da
Colin Gillespie <csgillespie@gmail.com>
2016-12-17 21:50:04 +0000

    Formatting

--- R/BIC_gambin.R
+++ R/BIC_gambin.R
@@ -1,8 +1,7 @@
 #' @rdname logLik.gambin
 #' @importFrom stats BIC
 #' @export
-BIC.gambin <-
-function(object, ...)
+BIC.gambin = function(object, ...)
 {
   if (length(list(...)) > 0L) 
     warning("additional arguments ignored")
--- R/confint_gambin.R
+++ R/confint_gambin.R
@@ -1,11 +1,12 @@
-est_confint  = function(est_alpha, est_likelihood, mydata, level)
-{
-  conf_logLik <- function(alpha, mydata, est_likelihood, level)
+est_confint  = function(est_alpha, est_likelihood, mydata, level) {
+  
+  conf_logLik = function(alpha, mydata, est_likelihood, level)
   {
-    return(abs(logLik_gamBin(alpha, mydata) - est_likelihood - exp(-qchisq(level,1)/2))) # the likelihood ratio test for confidence intervals (from "Beyond Traditional Statistical Measures")
+    # the likelihood ratio test for confidence intervals (from "Beyond Traditional Statistical Measures")
+    return(abs(logLik_gamBin(alpha, mydata) - est_likelihood - exp(-qchisq(level,1)/2))) 
   }
-  lower <- optimise(conf_logLik, interval = c(0,est_alpha), mydata = mydata, est_likelihood = est_likelihood, level = level)$minimum
-  higher <- optimise(conf_logLik, interval = c(est_alpha, 30), mydata = mydata, est_likelihood = est_likelihood, level = level)$minimum
+  lower = optimise(conf_logLik, interval = c(0,est_alpha), mydata = mydata, est_likelihood = est_likelihood, level = level)$minimum
+  higher = optimise(conf_logLik, interval = c(est_alpha, 30), mydata = mydata, est_likelihood = est_likelihood, level = level)$minimum
   return(c(lower, higher))
 }
 
