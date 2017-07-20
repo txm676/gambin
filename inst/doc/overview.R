f3af8c099c3860ef2bb4ef21691635643322bd71
txm676@gmail.com <txm676@gmail.com>
2017-07-20 21:58:17 +0100

    vignette edits for CRAN

--- inst/doc/overview.R
+++ inst/doc/overview.R
@@ -1,22 +1,36 @@
-## ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------
 library("gambin")
 data(moths, package="gambin")
 
 ##unimodal model
 fit = fit_abundances(moths)
+fit$alpha
 barplot(fit)
 points(fit)
 AIC(fit)
 
 ##unimodal model (fit to a subsample of 1000 individuals)
 fit2 = fit_abundances(moths, subsample = 1000)
+fit2$alpha
 barplot(fit2)
 points(fit2)
 AIC(fit2)
 
 ##bimodal model (using 3 cores)
-fit3 = fit_abundances(moths, no_of_components = 2, cores = 3)
+
+#simulate bimodal gambin distribution
+x1 = rgambin(600, 5, 10)
+x2 = rgambin(300, 1, 10)
+x = table(c(x1,x2))
+freq = as.vector(x)
+values = as.numeric(as.character(names(x)))
+abundances = data.frame(octave=values, species = freq)
+
+#fit bimodal model to simulated data
+fit3 = fit_abundances(abundances, no_of_components = 2, cores = 1)
 barplot(fit3)
 points(fit3)
 AIC(fit3)
+#compare with AIC of unimodal model
+AIC(fit_abundances(abundances))
 
