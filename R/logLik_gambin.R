5b2de090016f5b78d4c880d15105cc37eba763c8
Colin Gillespie <csgillespie@gmail.com>
2018-06-12 17:18:22 +0100

    Code cleaning

--- R/logLik_gambin.R
+++ R/logLik_gambin.R
@@ -11,8 +11,8 @@ ll_w = function(alpha, w, maxoctave, values, freq) {
 ## Then sanity checks before passing on to ll_w
 ll_optim = function(par, maxoctave, values, freq) {
   len_p = (length(par) + 1)/2
-  alpha=par[1:len_p]; w=par[(len_p+1):(2*len_p)]
-  w[len_p] = 1 - sum(w, na.rm=TRUE)
+  alpha = par[1:len_p]; w = par[(len_p+1):(2*len_p)]
+  w[len_p] = 1 - sum(w, na.rm = TRUE)
   if(any(alpha < 0) || any(w < 0) || any(w > 1)) return(Inf)
   if(length(alpha) != length(maxoctave)) stop("alpha and maxoctave should be the same length")
   #w = w/sum(w)
@@ -54,7 +54,7 @@ estimate_parameters = function(par, values, freq, cores = NULL) {
   i = 1 #Dummy line for RStudio warnings
   ## Run in parallel
   res = foreach(i=1:nrow(octaves))  %dopar% {
-    optim(par = par, fn=ll_optim,
+    optim(par = par, fn = ll_optim,
           maxoct = octaves[i,],
           values = values, freq = freq)
   }
